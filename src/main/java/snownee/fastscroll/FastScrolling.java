ad2220ccd24888693bd3d9cec6d23f31a13ae6c4
Snownee <i@snownee.com>
2022-11-13 10:51:09 +0800

    Forge port

---
+++ src/main/java/snownee/fastscroll/FastScrolling.java
@@ -0,0 +1,10 @@
+package snownee.fastscroll;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FastScrolling.ID)
+public class FastScrolling {
+
+	public static final String ID = "fastscroll";
+
+}
