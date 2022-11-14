6affc7ddc950cf183c3a7ff41cb0123daea094bc
Snownee <i@snownee.com>
2022-11-15 02:30:07 +0800

    fix: mark is client side

--- src/main/java/snownee/fastscroll/FastScrolling.java
+++ src/main/java/snownee/fastscroll/FastScrolling.java
@@ -1,10 +1,17 @@
 package snownee.fastscroll;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(FastScrolling.ID)
 public class FastScrolling {
 
 	public static final String ID = "fastscroll";
+
+	public FastScrolling() {
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
 
 }
